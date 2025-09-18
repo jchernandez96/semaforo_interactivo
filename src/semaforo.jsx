9dfab41820ac08dab9c22963d4b75f712b9cb6e3
jchernandez96 <jc.hernandez11@ciaf.edu.co>
2025-09-17 20:48:58 -0500

    Css

--- src/semaforo.jsx
+++ src/semaforo.jsx
@@ -5,19 +5,19 @@ export default function Semaforo({ setColorSemaforo }) {
   return (
     <div className="d-flex flex-wrap justify-content-center gap-2">
       <button
-        className="btn btn-outline-primary mb-4 mt-4"
+        className="btn btn-outline-danger mb-4 mt-4"
         onClick={() => setColorSemaforo('#FF0000 ')}
       >
         ROJO
       </button>
       <button
-        className="btn btn-outline-primary mb-4 mt-4"
+        className="btn btn-outline-warning mb-4 mt-4"
         onClick={() => setColorSemaforo('#FFFF00')}
       >
         AMARILLO
       </button>
       <button
-        className="btn btn-outline-primary mb-4 mt-4"
+        className="btn btn-outline-success mb-4 mt-4"
         onClick={() => setColorSemaforo('#00FF00')}
       >
         VERDE
